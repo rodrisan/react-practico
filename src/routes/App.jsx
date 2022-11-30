2abb3c7f2bcfbd0f8a82ddb484a02a8a128fe69a
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2022-11-30 16:51:57 -0600

    add routes

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -3,19 +3,21 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Layout from '../containers/Layout';
 import Login from '../containers/Login';
 import RecoverPassword from '../containers/RecoverPassword';
+import Home from '../pages/Home';
+import NotFound from '../pages/NotFound';
 import '../styles/global.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Layout>
+      <Layout>
+        <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
-          <Route exact path="/recovery-password" component={RecoverPassword} />
-          <Route component={NotFound} />
-        </Layout>
-      </Switch>
+          <Route exact path="/recover-password" component={RecoverPassword} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </Layout>
     </BrowserRouter>
   )
 }
