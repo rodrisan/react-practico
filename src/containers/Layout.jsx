c79d3053321a368d574b69436f8d4fca6c328099
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2022-11-30 16:05:49 -0600

    create default Layout container

---
+++ src/containers/Layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Layout({ children }) {
+    return (
+        <div className='layout'>
+            {children}
+        </div>
+    )
+}
+
+export default Layout
