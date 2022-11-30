09c8fcae107ed8eccfb28b1f9e1f665b7ab756f8
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2022-11-30 15:54:31 -0600

    correct settings for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,10 @@ module.exports = {
         })
     ],
     devServer: {
-        contentBase: path.join(__dirname, 'dist'),
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
         port: 3011,
     }
 }
