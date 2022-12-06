8fa19287bb77659d2357dda22413fa76ab22d464
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2022-12-06 08:56:17 -0600

    connect context to cart icon details

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import '@styles/Header.scss';
 
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
 import shoppingCart from '@icons/icon_shopping_cart.svg';
 import Menu from '@components/Menu';
+import AppContext from '@context/AppContext';
 
 const Header = () => {
 	const [toggle, setToggle] = useState(false);
+	const { state: { cart } } = useContext(AppContext);
 
 	const handleToggle = () => {
 		setToggle(!toggle);
@@ -47,7 +49,7 @@ const Header = () => {
 					</li>
 					<li className="navbar-shopping-cart">
 						<img src={shoppingCart} alt="shopping cart" />
-						<div>2</div>
+						{cart.length > 0 && <div>{cart.length}</div>}
 					</li>
 				</ul>
 			</div>
