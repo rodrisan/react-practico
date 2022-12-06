fdf7c9969a2b0602ec70de006e5c6bd7ed2d2288
Rodrigo Sarmiento <info@rodrigosarmiento.com>
2022-12-06 08:45:44 -0600

    update alias

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Layout from '../containers/Layout';
-import Home from '../pages/Home';
-import Login from '../pages/Login';
-import PasswordRecovery from '../pages/PasswordRecovery';
-import SendEmail from '../pages/SendEmail';
-import NewPassword from '../pages/NewPassword';
-import MyAccount from '../pages/MyAccount';
-import CreateAccount from '../pages/CreateAccount';
-import Checkout from '../pages/Checkout';
-import Orders from '../pages/Orders';
-import NotFound from '../pages/NotFound';
-import '../styles/global.css';
+import Layout from '@containers/Layout';
+import Home from '@pages/Home';
+import Login from '@pages/Login';
+import PasswordRecovery from '@pages/PasswordRecovery';
+import SendEmail from '@pages/SendEmail';
+import NewPassword from '@pages/NewPassword';
+import MyAccount from '@pages/MyAccount';
+import CreateAccount from '@pages/CreateAccount';
+import Checkout from '@pages/Checkout';
+import Orders from '@pages/Orders';
+import NotFound from '@pages/NotFound';
 import AppContext from '../context/AppContext';
 import useInitialState from '../hooks/useInitialState';
+import '@styles/global.css';
 
 const App = () => {
 	const initialState = useInitialState();
